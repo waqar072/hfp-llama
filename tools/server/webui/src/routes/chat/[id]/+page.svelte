15c3c91abf90b8002729739b883a8225838b2e4d
Waqar <ahmu751@gmail.com>
2026-01-02 16:34:42 +0530

    Added HFP logo

--- tools/server/webui/src/routes/chat/[id]/+page.svelte
+++ tools/server/webui/src/routes/chat/[id]/+page.svelte
@@ -164,7 +164,7 @@
 </script>
 
 <svelte:head>
-	<title>{activeConversation()?.name || 'Chat'} - llama.cpp</title>
+	<title>{activeConversation()?.name || 'Chat'} - HFP</title>
 </svelte:head>
 
 <ChatScreen />
